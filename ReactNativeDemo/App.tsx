8ab0b377a0820cd52f9764041538f86c206ec175
Dmitriy <dv.gromov1207@gmail.com>
2024-04-15 16:21:38 +0300

    upload react native demo

---
+++ ReactNativeDemo/App.tsx
@@ -0,0 +1,161 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+import {useEffect, useState} from 'react';
+import {NativeEventEmitter, NativeModules} from 'react-native';
+const {DGChatModule} = NativeModules;
+
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+useEffect(() => {
+    const eventEmitter = new NativeEventEmitter(NativeModules.DGChatModule);
+    let onChatMinimizeClickEventListener = eventEmitter.addListener('OnChatMinimizeClick', event => {
+      DGChatModule.showToast("OnChatMinimizeClick")
+    });
+    let onChatEndClickEventListener = eventEmitter.addListener('onChatEndClick', event => {
+      DGChatModule.showToast("onChatEndClick")
+    });
+    let onChatLauncherClickEventListener = eventEmitter.addListener('onChatLauncherClick', event => {
+      DGChatModule.showToast("onChatLauncherClick")
+    });
+    let onChatProactiveButtonClickEventListener = eventEmitter.addListener('onChatProactiveButtonClick', event => {
+      DGChatModule.showToast("onChatProactiveButtonClick")
+    });
+
+
+    return () => {
+      onChatMinimizeClickEventListener.remove(); 
+	onChatEndClickEventListener.remove(); 
+	onChatLauncherClickEventListener.remove(); 
+	onChatProactiveButtonClickEventListener.remove(); 
+    };
+  }, []
+);
+
+
+    DGChatModule.showDGChatView(
+	"f08c2308-b8f9-4390-af00-da1724862787", 
+	"https://flow-server.eu.dgdeepai.com", 
+	"1.1.0", 
+	true, 
+	"metadata: { \"currentPage\": \"some-random-string\", \"currentPageTitle\": \"another-random-string\"}"
+    );
+
+
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
